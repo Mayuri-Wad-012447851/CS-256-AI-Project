d3968d1a5e1e4d74aaebe78e156a858468724619
Mayuri Wadkar <mayuri-wad-012447851@users.noreply.github.com>
2017-11-06 10:45:47 -0800

    updating Utils

--- Utils.py
+++ Utils.py
@@ -8,7 +8,6 @@ class Utils(object):
     #This method builds a job document term matrix
     def buildCorpus(self):
         Corpus = {}
-        ReversedCorpus = {}
 
         for job in Webscraper.jobs_fetched:
             for word in job.summary:
@@ -18,10 +17,14 @@ class Utils(object):
                         frequencies.append(0)
                     Corpus[word] = frequencies
                 Corpus[word][job.id] += 1
+        self.TF_Corpus = Corpus
 
-        terms = Corpus.keys()
+    # reversing the corpus
+    def buildReversedCorpus(self):
+        ReversedCorpus = {}
 
-        # reversing the corpus
+        terms = self.TF_Corpus.keys()
+
         for i in range(0, len(Webscraper.jobs_fetched)):
             array_termFrequency = []
             for k in range(0, len(terms)):
@@ -33,20 +36,30 @@ class Utils(object):
         for jobIdkey in jobIDs:
             for j in range(0, len(terms)):
                 word = terms[j]
-                ReversedCorpus[jobIdkey][j] = Corpus[word][jobIdkey]
+                ReversedCorpus[jobIdkey][j] = self.TF_Corpus[word][jobIdkey]
+
+        self.TF_Reversed_Corpus = ReversedCorpus
+
+    #computing all tf documents
+    def compute_TF_documents(self):
 
         for job in Webscraper.jobs_fetched:
-            termFreq = ReversedCorpus[job.id]
+            termFreq = self.TF_Reversed_Corpus[job.id]
             # finding TF for each job document
             for k in range(0, len(termFreq)):
                 var1 = termFreq[k]
-                var2 = sum(ReversedCorpus[job.id])
+                var2 = sum(self.TF_Reversed_Corpus[job.id])
                 job.TFvector.append(float(var1 / var2))
 
-        #calculating IDFs of all terms
+    # calculating IDFs of all terms
+    def compute_IDFs(self):
+
+        terms = self.TF_Corpus.keys()
+        jobIDs = self.TF_Reversed_Corpus.keys()
+
         idf_terms = {}
         for term in terms:
-            termVector = Corpus[term]
+            termVector = self.TF_Corpus[term]
             occurance = 0
             for k in range(0,len(termVector)):
                 if termVector[k] != 0:
@@ -60,23 +73,6 @@ class Utils(object):
                 val2 = float(job.TFvector[i])
                 job.TF_IDF.append(float(val * val2))
 
-        self.TF_Corpus = Corpus
-        self.TF_Reversed_Corpus = ReversedCorpus
+
+
         self.term_IDFs = idf_terms
-
-    #This method normalizes term frequency job document
-    def normalizePercept(self, vector):
-        factor = 0
-        NormalizedVector = []
-
-        for val in vector:
-            factor += val
-        for val in vector:
-            NormalizedVector.append(float(val/factor))
-
-        terms = self.TF_Corpus.keys()
-        perceptIDF = []
-        for i in range(0,len(vector)):
-            perceptIDF.append(float(NormalizedVector[i]*self.term_IDFs[str(terms[i])]))
-
-        return perceptIDF
